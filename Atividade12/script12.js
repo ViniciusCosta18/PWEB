00f2ed907c3a7398d73fa26813fc3701c2f29eb2
ViniciusCosta18 <88666411+ViniciusCosta18@users.noreply.github.com>
2023-05-03 21:40:23 -0300

    Add files via upload

---
+++ Atividade12/script12.js
@@ -0,0 +1,11 @@
+function janelaFechada(obj) {
+    obj.style.backgroundImage = "url(https://madeireiraitauna.com.br/wp-content/uploads/2021/08/Janela-MI91140-Fechada.jpg)";
+}
+
+function janelaAberta(obj) {
+    obj.style.backgroundImage = "url(https://w7.pngwing.com/pngs/418/69/png-transparent-window-open-windows-angle-photography-windows.png)";
+}
+
+function janelaQuebrada(obj) {
+    obj.style.backgroundImage = "url(https://canalcienciascriminais.com.br/wp-content/uploads/2019/05/windows.png)";
+}
